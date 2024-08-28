9885658111967f2ef9d8bb6dbeb9e1339fd94fe7
Vlad Yashenkov <yashenkov.v@yandex.ru>
2024-08-28 15:04:34 +0300

    test: test PROMO_ID

--- handlers/base.py
+++ handlers/base.py
@@ -61,6 +61,7 @@ async def main_page(message: Union[types.Message, types.CallbackQuery],
 async def show_promo(message: types.Message,
                      bot: Bot,
                      state: FSMContext):
+    print(PROMO_ID)
     await message.answer_video(PROMO_ID,
                                disable_notification=True,
                                reply_markup=create_close_kb('promo').as_markup())
